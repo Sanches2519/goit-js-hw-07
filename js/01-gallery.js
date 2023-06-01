a7f9363a191c46dac17aedeae907cabda3b36a85
Sanches2519 <dolgoruk.olexsandr@gmail.com>
2023-06-01 23:54:08 +0300

    d

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", onClick);
+
+const imagesEl = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-sourse="${original}" alt="${description}">
+      </a>
+    </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", imagesEl);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.closest(".gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.sourse}">
+`);
+  instance.show();
+  gallery.addEventListener("keydown", onKey);
+  function onKey(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", onClick);
+const imagesEl = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+    </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", imagesEl);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.closest(".gallery__image")) {
+    return;
+  }
+  }
 
-console.log(galleryItems);
+// console.log(galleryItems);
